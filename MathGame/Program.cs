668ea4eaea7941a54ebef075130d1b3489b639dc
Chris Thauer <cthauerdev@gmail.com>
2023-04-19 18:57:29 -0700

    First commit

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿string[] results;
+bool playing = true;
+string choice;
+
+Console.WriteLine("Welcome to the Math Game");
+Console.WriteLine("===========================");
+
+while (playing)
+{
+    Play();
+}
+void Play()
+{
+    Console.WriteLine("A) Addition");
+    Console.WriteLine("B) Subtraction");
+    Console.WriteLine("C) Multiplication");
+    Console.WriteLine("D) Division");
+    Console.WriteLine("E) View Results");
+    Console.WriteLine("F) Exit");
+    Console.Write("Please select an operation: ");
+    choice = Console.ReadLine().ToLower();
+
+    if (choice == "f")
+    {
+        playing = false;
+    }
+};
